0e03c37b69dedbef5798e7da974bb3cd02586200
RobsonGilRocha <gil1990gil@hotmail.com>
2021-06-12 22:25:24 -0300

    menu

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const readlineSync = require('readline-sync');
-function oi(){
-    let nome
-    nome = readlineSync.question('Digite o seu nome: ')
+const readlineSync = require('readline-sync'); 
+const csv = require('csvtojson') 
 
-    console.log(" \n \n HELLO WORLD",`${nome}`,"!!! \n \n \n ")
+
+
+
+function app(){
+    let op = 0
+
+    console.log('=== MENU ===') 
+    console.log('1. Listar todos os pontos de taxi') 
+    console.log('2. Informar minha localização') 
+    console.log('3. Encontrar pontos próximos')
+    console.log('4. Buscar pontos por logradouro')  
+    console.log('5. Terminar o programa\n')
+
+    op = readlineSync.question('Escolha uma das opções: ');
+
+    console.log(op)
 }
-oi()
+app()
