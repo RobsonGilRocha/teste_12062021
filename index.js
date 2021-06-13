d8b90ff1d7355ab400e0af1f796af57d797eeadd
RobsonGilRocha <gil1990gil@hotmail.com>
2021-06-12 22:37:43 -0300

    final menu

--- index.js
+++ index.js
@@ -7,15 +7,38 @@ const csv = require('csvtojson')
 function app(){
     let op = 0
 
-    console.log('=== MENU ===') 
-    console.log('1. Listar todos os pontos de taxi') 
-    console.log('2. Informar minha localização') 
-    console.log('3. Encontrar pontos próximos')
-    console.log('4. Buscar pontos por logradouro')  
-    console.log('5. Terminar o programa\n')
+    while(true){
+        console.log('=== MENU ===') 
+        console.log('1. Listar todos os pontos de taxi') 
+        console.log('2. Informar minha localização') 
+        console.log('3. Encontrar pontos próximos')
+        console.log('4. Buscar pontos por logradouro')  
+        console.log('5. Terminar o programa\n')
+    
+        op = readlineSync.question('Escolha uma das opções: ');
+    
+        if (op == 1) { 
+            console.log(op, "foi selecionada") 
+      
+          } else if (op == 2) { 
+            console.log(op, "foi selecionada")  
+      
+          } else if (op == 3) { 
+            console.log(op, "foi selecionada")  
+      
+          } else if (op == 4) { 
+            console.log(op, "foi selecionada")  
+      
+          } else if (op == 5) { 
+            console.log(op, "foi selecionada")  
+      
+          }
+        
+          
+          break
+    }
 
-    op = readlineSync.question('Escolha uma das opções: ');
 
-    console.log(op)
+    
 }
 app()
